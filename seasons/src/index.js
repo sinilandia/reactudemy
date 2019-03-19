2eaf01c4c5b042fc05ee1cf53db40a80532bfc6b
sinilandia <aisinilandia@gmail.com>
2019-03-19 20:43:52 +0200

    refactor seasons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
 
-    return (
-        <div>Latitude: </div>
-    )       
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+        
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    render() {
+        return (
+        <div>
+            Latitude: {this.state.lat} <br />
+            Error: {this.state.errorMessage}
+        </div>
+        );
+    }
+}
 
 
 
